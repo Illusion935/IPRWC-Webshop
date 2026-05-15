103200d86e43cbe4be205b52acd69ef69077944c
Your Name <you@example.com>
2026-05-15 19:39:46 +0200

    feat: add navbar component with admin and login links

---
+++ Frontend/src/app/components/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class NavbarComponent {
+  constructor(public authService: AuthService) {}
+}
